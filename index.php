37382f70cd6bdcc6fcfad8bed0294fdf979b1070
IanSurii <mrkeys254@gmail.com>
2021-03-26 20:18:49 +0300

    visitor_count

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php
-include('index.html');
+include('page.html');
+include 'classes/DbFunctions.php';
+$visitor_ip = $_SERVER['REMOTE_ADDR'];
+$execute=new dbFunction();
+$column="ip,time";
+$values="'$visitor_ip',CURRENT_TIME";
+$insert_visitor=$execute->insert("portofolio_visitors",$column,$values);
+
+
 ?>
