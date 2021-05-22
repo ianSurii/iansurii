a584c6148c852542fe11970865d49b0f7d9bd2f8
IanSurii <mrkeys254@gmail.com>
2021-05-22 14:51:10 +0300

    master

--- api.php
+++ api.php
@@ -1,5 +1,5 @@
 <?php
-$resposnse={'id': 1,'profilepicUrl':'https://github.com/ianSurii/100DaysOfCodeProjects/blob/master/images/Overwatch.jpeg','user': 'user','time': 'time','location': 'location','game_picture': 'game_picture','exchange1': 'exchange1','exchange2': 'exchange2','exchange3': 'exchange3'}
+$resposnse="'id': 1,'profilepicUrl':'https://github.com/ianSurii/100DaysOfCodeProjects/blob/master/images/Overwatch.jpeg','user': 'user','time': 'time','location': 'location','game_picture': 'game_picture','exchange1': 'exchange1','exchange2': 'exchange2','exchange3': 'exchange3'}";
       echo $resposnse
 
       ?>
