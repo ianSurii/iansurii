bdde66aaa7dbe6d647dd6578af7f743641a4bd74
IanSurii <mrkeys254@gmail.com>
2021-05-22 14:37:30 +0300

    master

---
+++ api.php
@@ -0,0 +1,13 @@
+<?php
+$resposnse="'id': 1,
+      'profilepicUrl': https://github.com/ianSurii/100DaysOfCodeProjects/blob/master/images/Assassin's_Creed_Valhalla.jpg,
+      'user': user,
+      'time': time,
+      'location': location,
+      'game_picture': game_picture,
+      'exchange1': exchange1,
+      'exchange2': exchange2,
+      'exchange3': exchange3,";
+      echo json_encode($resposnse);
+
+      ?>
