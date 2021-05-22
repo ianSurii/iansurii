7c2cdb19c0e9473985a175a0683e838f6d9e73f4
IanSurii <mrkeys254@gmail.com>
2021-05-22 14:43:53 +0300

    master

--- api.php
+++ api.php
@@ -1,13 +1,11 @@
 <?php
-$resposnse="'id': 1,
-      'profilepicUrl': https://github.com/ianSurii/100DaysOfCodeProjects/blob/master/images/Assassin's_Creed_Valhalla.jpg,
-      'user': user,
-      'time': time,
-      'location': location,
-      'game_picture': game_picture,
-      'exchange1': exchange1,
-      'exchange2': exchange2,
-      'exchange3': exchange3,";
-      echo json_encode($resposnse);
+$resposnse={'id': 1,'profilepicUrl':'https://github.com/ianSurii/100DaysOfCodeProjects/blob/master/images/Overwatch.jpeg','user': 'user',
+      'time': 'time',
+      'location': 'location',
+      'game_picture': 'game_picture',
+      'exchange1': 'exchange1',
+      'exchange2': 'exchange2',
+      'exchange3': 'exchange3'}
+      echo $resposnse
 
       ?>
