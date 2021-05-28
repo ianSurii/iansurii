f82b7ffc176ee696ed0985d6f1a34df7523e3bbe
IanSurii <mrkeys254@gmail.com>
2021-05-28 14:15:33 +0300

    images

--- api.php
+++ api.php
@@ -1,5 +1,10 @@
 <?php
-$resposnse="'id': 1,'profilepicUrl':'https://github.com/ianSurii/100DaysOfCodeProjects/blob/master/images/Overwatch.jpeg','user': 'user','time': 'time','location': 'location','game_picture': 'game_picture','exchange1': 'exchange1','exchange2': 'exchange2','exchange3': 'exchange3'}";
-      echo $resposnse
+
+include 'classes/DbFunctions.php';
+$execute=new dbFunction();
+$response=$execute->selectAll('gamerx');
+echo json_encode($response);
+
+
 
       ?>
