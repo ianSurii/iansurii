96836c47818695c71cb93cfabb0ba5a728100f2b
IanSurii <mrkeys254@gmail.com>
2021-06-19 10:32:01 +0300

    api

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
 include('page.html');
-include 'classes/DbFunctions.php';
-$visitor_ip = $_SERVER['REMOTE_ADgDR'];
-$execute=new dbFunction();
-$column="ip,time";
-$values="'$visitor_ip',CURRENT_TIME";
-$insert_visitor=$execute->insert("portofolio_visitors",$column,$values);
+// include 'classes/DbFunctions.php';
+// $visitor_ip = $_SERVER['REMOTE_ADgDR'];
+// $execute=new dbFunction();
+// $column="ip,time";
+// $values="'$visitor_ip',CURRENT_TIME";
+// $insert_visitor=$execute->insert("portofolio_visitors",$column,$values);
 
 
 ?>
